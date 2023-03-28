9a2b57b92cc1de837df6d94087689b5a621ca117
R-Grigala <r.grigalashvili777@gmail.com>
2023-03-28 13:15:26 +0400

    Coding Continue:// add Date format in origin_time

--- components/eqList/EqList.js
+++ components/eqList/EqList.js
@@ -1,12 +1,26 @@
 import { View, FlatList, RefreshControl } from 'react-native';
 import React from 'react';
-// import { EQ_DATA } from '../../data/EqData';
 import EqItem from './EqItem';
 
 function EqList({data, onRefresh}){
 
     const renderItem = ({item}) => {
-        return <EqItem eqId={item.id} origin_time={item.origin_time} ml={item.ml} depth={item.depth} latitude={item.latitude} longitude={item.longitude} description={item.description}/>
+
+        // Create a new Date object from the date string
+        const dateObj = new Date(item.origin_time);
+
+        // Extract the date components from the date object
+        const year = dateObj.getFullYear();
+        const month = dateObj.getMonth() + 1; // Add 1 to account for zero-based indexing
+        const date = dateObj.getDate();
+        const hours = dateObj.getHours();
+        const minutes = dateObj.getMinutes();
+        const seconds = dateObj.getSeconds();
+
+        // Construct a new date string in the desired format
+        const newDateString = `${date}/${month}/${year}  ${hours}:${minutes}:${seconds}`;
+
+        return <EqItem eqId={item.id} origin_time={newDateString} ml={item.ml} depth={item.depth} latitude={item.latitude} longitude={item.longitude} description={item.description}/>
     }
 
     return (

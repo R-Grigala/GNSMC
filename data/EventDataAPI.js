83c928a56b451ec733a204957ac9089be956aa3a
R-Grigala <r.grigalashvili777@gmail.com>
2023-05-15 16:10:02 +0400

    Coding Grigala:// add newsposts API

--- data/EventDataAPI.js
+++ data/EventDataAPI.js
@@ -1,15 +1,28 @@
 const EventDataAPI = async() =>{
+
+  const token = 'ee1403e4ef95d1c81a5ee47d864bd646fea308e7'; // Replace with the actual token
+
+  try {
+    const response = await fetch('http://10.0.2.2:8000/api/events/', {
+      headers: {
+        Authorization: `Token ${token}`
+      }
+    });
     
-    try {
-      const response = await fetch('http://10.0.2.2:8000/api/events/');
-      
+    if (response.ok) {
       const data = await response.json();
+      // Process the data received from the server
       // console.log("Call EventData");
       return data;
-
-    } catch (error) {
-        console.log(error.message);
+    } else {
+      const error = await response.json();
+      // Handle the error response from the server
+      console.log('Error:', error);
     }
+
+  } catch (error) {
+      console.log(error.message);
+  }
 
   }
   
---
+++ data/NewsDataAPI.js
@@ -0,0 +1,29 @@
+const NewsDataAPI = async() =>{
+
+    const token = 'ee1403e4ef95d1c81a5ee47d864bd646fea308e7'; // Replace with the actual token
+  
+    try {
+      const response = await fetch('http://10.0.2.2:8000/api/newsposts/', {
+        headers: {
+          Authorization: `Token ${token}`
+        }
+      });
+      
+      if (response.ok) {
+        const data = await response.json();
+        // Process the data received from the server
+        console.log("Call EventData");
+        return data;
+      } else {
+        const error = await response.json();
+        // Handle the error response from the server
+        console.log('Error:', error);
+      }
+  
+    } catch (error) {
+        console.log(error.message);
+    }
+  
+    }
+    
+  export default NewsDataAPI;

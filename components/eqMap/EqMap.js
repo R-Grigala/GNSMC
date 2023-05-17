4dd731707ee52c4d4e8c9ef4f7cb625a0f589a83
R-Grigala <r.grigalashvili777@gmail.com>
2023-05-17 17:57:47 +0400

    Coding Grigala:// ChatGPT adition

--- components/eqMap/EqMap.js
+++ components/eqMap/EqMap.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, TouchableOpacity, View, Image} from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View, Image } from 'react-native';
 import React from 'react';
 import MapView, { Callout, Marker } from 'react-native-maps';
 import { EQ_DATA } from '../../data/EqData';
@@ -7,25 +7,26 @@ import { Ionicons } from '@expo/vector-icons';
 
 const EqMap = () => {
   const navigation = useNavigation();
+
   return (
     <MapView 
-        style={styles.mapview}
-        initialRegion={{
+      style={styles.mapview}
+      initialRegion={{
         latitude: 42.0186,
         longitude: 43.9911,
         latitudeDelta: 10,
         longitudeDelta: 10,
-        }}
-        mapType="hybrid"
-        showsMyLocationButton={false}
-        showsUserLocation={false}
+      }}
+      mapType="hybrid"
+      showsMyLocationButton={false}
+      showsUserLocation={false}
     >
       {EQ_DATA.map((event, index) => (
         <Marker 
           key={index} 
           coordinate={{
-            latitude:event.latitude,
-            longitude:event.longitude,
+            latitude: event.latitude,
+            longitude: event.longitude,
           }}
         >
           <Image
@@ -34,39 +35,46 @@ const EqMap = () => {
           />
           <Callout>
             <TouchableOpacity 
-              onPress={()=> navigation.navigate(
+              onPress={() => navigation.navigate(
                 "MapDetailScreen", 
-                {eqId: event.id, origin_time: event.origin_time, ml: event.ml, depth: event.depth, description: event.description}
+                {
+                  eqId: event.id,
+                  origin_time: event.origin_time,
+                  ml: event.ml,
+                  depth: event.depth,
+                  description: event.description
+                }
               )}
-              >
-              <View style={{ flexDirection: 'row', alignItems: 'center', width:290, height:75}}>
-                <Text style={{padding:0, margin:0}}>
+            >
+              <View style={{ flexDirection: 'row', alignItems: 'center', width: 290, height: 75 }}>
+                <Text style={{ padding: 0, margin: 0 }}>
+                  {/* Display earthquake details */}
                   <Text style={{ fontSize: 15, fontWeight: 'bold' }}>დრო(UTC): </Text>{event.origin_time}
                   <Text style={{ fontSize: 15, fontWeight: 'bold' }}>{'\n'}გან/გრძ: </Text>{event.latitude} / {event.longitude}
                   <Text style={{ fontSize: 15, fontWeight: 'bold' }}>{'\n'}მაგნიტუდა: </Text>{event.ml}
                   <Text style={{ fontSize: 15, fontWeight: 'bold' }}>{'\n'}DEPTH: </Text>{event.depth}
                 </Text>
-                <View style={{marginBottom:18, marginTop:17,marginLeft:30}}>
-                  <Ionicons name='alert-circle-outline' size={40} color='#4083ff'/>
+                <View style={{ marginBottom: 18, marginTop: 17, marginLeft: 30 }}>
+                  {/* Display alert icon */}
+                  <Ionicons name='alert-circle-outline' size={40} color='#4083ff' />
                 </View>
               </View>
             </TouchableOpacity>
           </Callout>
         </Marker>
       ))}
-  </MapView>
+    </MapView>
   );
 };
 
 const styles = StyleSheet.create({
-
   mapview: {
-    flex:5,
+    flex: 1,
   },
   markerIcon: {
-    width:20,
-    height:20
+    width: 20,
+    height: 20
   }
-  });
+});
 
-export default EqMap
+export default EqMap;

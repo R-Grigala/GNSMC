410b4c6e94ef3c3424ba7d2799eaa4f8adb5ee8b
R-Grigala <r.grigalashvili777@gmail.com>
2023-06-07 17:45:05 +0400

    Coding Continue:// News Detail Screen

--- components/news/NewsDetail.js
+++ components/news/NewsDetail.js
@@ -1,14 +1,89 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { useRoute } from '@react-navigation/native'
+import React from 'react';
+import { SafeAreaView, StyleSheet, Text, Image, View, ScrollView } from 'react-native';
+import { formatData } from '../../utils/formatData';
+import { useRoute } from '@react-navigation/native';
 
 const NewsDetail = () => {
   const route = useRoute()
+
+  const { urlImage, title, description, uploadTime } = route.params
+
   return (
-    <View>
-      <Text>NewsDetail</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+        <View style={styles.viewimg}>
+          {/* Image */}
+          <Image source={{
+              uri: urlImage
+            }} 
+            style={styles.image}
+          />
+        </View>
+        <View style={{padding: 15}}>
+          {/* Title */}
+          <View style={styles.viewtext}>
+            <Text style={styles.title} >{title}</Text>
+          </View>
+          
+          {/* Description */}
+          <Text style={styles.description}>{description}</Text>
+          <View style={styles.data}>
+              <View style={styles.date}>
+              {/* Formatted Upload Time */}
+              <Text style={styles.text}>{formatData(uploadTime)}</Text>
+              </View>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
-export default NewsDetail
+const styles = StyleSheet.create({
+    container:{
+      flex: 1,
+    },
+    image:{
+      height: 200,
+      width: '100%',
+    },
+    title:{
+      fontSize: 18,
+      fontWeight: '600',
+      marginBottom: 10,
+    },
+    description:{
+      fontSize: 16,
+      fontWeight: '400',
+      marginTop: 10,
+    },
+    data:{
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      marginTop: 10,
+      alignItems: 'center',
+    },
+    date:{
+      flex:1,
+      alignItems: 'flex-end',
+    },
+    text:{
+      textAlign: 'right',
+      color: '#e63946',
+      fontWeight: 'bold',
+      fontSize: 15,
+    },
+    viewimg: {
+      flex:1,
+      marginBottom:10,
+      borderTopWidth: 1,
+      borderBottomWidth: 3,
+    },
+    viewtext: {
+      flex: 1,
+      marginBottom:10,
+      borderBottomWidth: 1.5,
+    }
+})
+
+export default NewsDetail;
--- screens/NewsDetailScreen.js
+++ screens/NewsDetailScreen.js
@@ -1,11 +1,16 @@
-import { SafeAreaView, Text, StyleSheet } from 'react-native'
+import { SafeAreaView, View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 import NewsDetail from '../components/news/NewsDetail'
 
 const NewsDetailScreen = () => {
   return (
     <SafeAreaView style={styles.screen}>
-      <NewsDetail />
+      <View style={styles.content}>
+        <Text style={styles.header}>მიმდინარე სეისმურობა</Text>
+      </View>
+      <View style={styles.newsContainer} >
+        <NewsDetail />
+      </View>
     </SafeAreaView>
   )
 }
@@ -13,6 +18,24 @@ const NewsDetailScreen = () => {
 const styles = StyleSheet.create({
     screen: {
       flex: 1,
+    },
+    content: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'flex-start',
+      borderWidth: 0.5,
+      borderColor: 'rgba(0,0,0,0.3)',
+      backgroundColor: 'rgba(0,0,0,0.3)',
+    },
+    header: {
+      flex: 1,
+      color: 'rgba(122, 0, 2, 1)',
+      marginLeft: 20,
+      marginTop: 8,
+      fontWeight: 'bold',
+    },
+    newsContainer: {
+      flex: 20,
     },
   })
 

a5257872fdbc0fa3c96780e94c6f986f73ff1e16
R-Grigala <r.grigalashvili777@gmail.com>
2023-06-19 18:27:26 +0400

    Coding Continue:// Add Settings file

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,11 +1,11 @@
-import { View, Text } from 'react-native';
+import { View, Text, SafeAreaView } from 'react-native';
 import React from 'react';
 
 const SettingsScreen = () => {
   return (
-    <View>
-      <Text>SettingsScreen</Text>
-    </View>
+    <SafeAreaView>
+      
+    </SafeAreaView>
   )
 }
 

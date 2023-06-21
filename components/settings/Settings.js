565b71801fc3f6b588c956c98be2012acf044a0f
R-Grigala <r.grigalashvili777@gmail.com>
2023-06-22 00:35:16 +0400

    Coding Grigala:// Change Settings file

--- components/settings/Settings.js
+++ components/settings/Settings.js
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native';
+import {ScrollView, StyleSheet, View, Text } from 'react-native';
 import React from 'react';
 
 const SECTIONS = [
@@ -7,24 +7,47 @@ const SECTIONS = [
     items: [
       { id: 'language', icon: 'globe', label: 'Language', type: 'select' },
       { id: 'darkMode', icon: 'moon', label: 'Dark Mode', type: 'toggle' },
-      { id: 'wife', icon: 'wife', label: 'use Wi-Fi', type: 'toggle' },
+      { id: 'notifications', icon: 'notifications-outline', label: 'use notifications', type: 'toggle' },
     ],
   },
   {  
-    header: 'Help',
+    header: 'About',
     items: [
-      { id: 'bug', icon: 'flag', label: 'Report Bug', type: 'link' },
-      { id: 'contact', icon: 'mail', label: 'Contact Us', type: 'link' },
+      { id: 'about', icon: 'help-circle-outline', label: 'About Us', type: 'link' },
+      { id: 'contact', icon: 'mail-outline', label: 'Contact Us', type: 'link' },
     ],
   },
 ]
 
 const Settings = () => {
   return (
-    <View>
-      <Text>Settings</Text>
-    </View>
+    <ScrollView contentContainerStyle={styles.container}>
+      {SECTIONS.map(({ header, items }) => (
+        <View style={styles.section} key={header}>
+          <View style={styles.sectionHeader}>
+            <Text style={styles.sectionHeaderText}>{header}</Text>
+          </View>
+        </View>
+      ))}
+    </ScrollView>
   )
 }
 
-export default Settings
+const styles = StyleSheet.create({
+  container: {
+    paddingVertical: 24,
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: '700',
+    color: '#1d1d1d',
+    marginBottom: 6,
+  },
+  subtitle: {
+    fontSize: 15,
+    fontWeight: '500',
+    color: '#929292',
+  }
+});
+
+export default Settings;
--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,12 +1,42 @@
-import { View, Text, SafeAreaView } from 'react-native';
+import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
 import React from 'react';
+import Settings from '../components/settings/Settings';
 
 const SettingsScreen = () => {
   return (
-    <SafeAreaView>
-      
+    <SafeAreaView style={styles.container}>
+      <View style={styles.content}>
+        <Text style={styles.header}>პარამეტრების განახლება</Text>
+      </View>
+      <View style={styles.setContainer}>
+        <Settings />
+      </View>
     </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+    borderWidth: 0.5,
+    borderColor: 'rgba(0,0,0,0.3)',
+    backgroundColor: 'rgba(0,0,0,0.3)',
+  },
+  header: {
+    flex: 1,
+    color: 'rgba(122, 0, 2, 1)',
+    marginLeft: 20,
+    marginTop: 8,
+    fontWeight: 'bold',
+  },
+  setContainer: {
+    flex: 20,
+  },
+});
 
 export default SettingsScreen;

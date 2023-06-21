373af141f7e3bde35e7325a8f10d0d9e66c80ea5
R-Grigala <r.grigalashvili777@gmail.com>
2023-06-22 01:16:00 +0400

    Coding Grigala:// Change Styles for Settings

--- components/settings/Settings.js
+++ components/settings/Settings.js
@@ -1,5 +1,6 @@
-import {ScrollView, StyleSheet, View, Text } from 'react-native';
-import React from 'react';
+import {ScrollView, StyleSheet, View, Text, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import { Ionicons } from '@expo/vector-icons';
 
 const SECTIONS = [
   {
@@ -7,7 +8,7 @@ const SECTIONS = [
     items: [
       { id: 'language', icon: 'globe', label: 'Language', type: 'select' },
       { id: 'darkMode', icon: 'moon', label: 'Dark Mode', type: 'toggle' },
-      { id: 'notifications', icon: 'notifications-outline', label: 'use notifications', type: 'toggle' },
+      { id: 'notifications', icon: 'notifications-outline', label: 'Notifications', type: 'toggle' },
     ],
   },
   {  
@@ -17,15 +18,52 @@ const SECTIONS = [
       { id: 'contact', icon: 'mail-outline', label: 'Contact Us', type: 'link' },
     ],
   },
-]
+];
 
 const Settings = () => {
+  const [form, setForm] = useState({
+    language: 'ქართული',
+    darkMode: true,
+    notications: true,
+  })
   return (
-    <ScrollView contentContainerStyle={styles.container}>
+    <ScrollView>
       {SECTIONS.map(({ header, items }) => (
         <View style={styles.section} key={header}>
           <View style={styles.sectionHeader}>
             <Text style={styles.sectionHeaderText}>{header}</Text>
+          </View>
+
+          <View style={styles.sectionBody}>
+            {items.map(({ label, id, type, icon }, index) => (
+              <View 
+                style={[
+                  styles.rowWrapper,
+                  index === 0 && { borderTopWindth: 0},
+                ]}
+                key={id}>
+                <TouchableOpacity
+                  onPress={() => {
+                    // handle onPress
+                  }}>
+                  <View style={styles.row}>
+                    <Ionicons
+                      name={icon}
+                      color='#000'
+                      size={28}
+                      style={{ marginRight: 12}}
+                    />
+                    <Text style={styles.rowLabel}>{label}</Text>
+
+                    <View style={styles.rowSpacer}/>
+
+                    {type === 'select' && (
+                      <Text style={styles.rowValue}>{form[id]}</Text>
+                    )}
+                  </View>
+                </TouchableOpacity>
+              </View>
+            ))}
           </View>
         </View>
       ))}
@@ -34,9 +72,6 @@ const Settings = () => {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    paddingVertical: 24,
-  },
   title: {
     fontSize: 32,
     fontWeight: '700',
@@ -47,6 +82,46 @@ const styles = StyleSheet.create({
     fontSize: 15,
     fontWeight: '500',
     color: '#929292',
+  },
+  section: {
+    paddingTop: 12,
+  },
+  sectionHeader: {
+    paddingHorizontal: 24,
+    paddingVertical: 8,
+  },
+  sectionHeaderText: {
+    fontSize: 14,
+    fontWeight: '600',
+    color: '#a7a7a7',
+    textTransform: 'uppercase',
+    letterSpacing: 1.2,
+  },
+  rowWrapper: {
+    paddingLeft: 24,
+    borderTopWidth: 1,
+    borderColor: '#e3e3e3',
+    backgroundColor: '#fff',
+  },
+  row: {
+    height: 50,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    paddingRight: 24,
+  },
+  rowLabel: {
+    fontSize: 17,
+    fontWeight: '500',
+    color: '#000'
+  },
+  rowSpacer: {
+    flex: 1, 
+  },
+  rowValue: {
+    fontSize: 17,
+    color: '#616161',
+    marginRight: 4,
   }
 });
 

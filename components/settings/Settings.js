186c7a9055e91606d93f62c9c4a869386612ec54
R-Grigala <r.grigalashvili777@gmail.com>
2023-06-21 23:52:04 +0400

    Coding Continue:// Add Sections for settings

--- components/settings/Settings.js
+++ components/settings/Settings.js
@@ -1,5 +1,23 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const SECTIONS = [
+  {
+    header: 'Preferences',
+    items: [
+      { id: 'language', icon: 'globe', label: 'Language', type: 'select' },
+      { id: 'darkMode', icon: 'moon', label: 'Dark Mode', type: 'toggle' },
+      { id: 'wife', icon: 'wife', label: 'use Wi-Fi', type: 'toggle' },
+    ],
+  },
+  {  
+    header: 'Help',
+    items: [
+      { id: 'bug', icon: 'flag', label: 'Report Bug', type: 'link' },
+      { id: 'contact', icon: 'mail', label: 'Contact Us', type: 'link' },
+    ],
+  },
+]
 
 const Settings = () => {
   return (

e7c2b58c01541676028af9004d39671cbfebfc58
R-Grigala <r.grigalashvili777@gmail.com>
2023-07-24 16:53:17 +0400

    Coding Continue:// EqMap components

---
+++ components/eqMap/EqMapFilter.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const EqMapFilter = () => {
+  return (
+    <View>
+      <Text>EqMapFilter</Text>
+    </View>
+  )
+}
+
+export default EqMapFilter
+
+const styles = StyleSheet.create({})
---
+++ components/eqMap/EqMapLegend.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const EqMapLegend = () => {
+  return (
+    <View>
+      <Text>EqMapLegend</Text>
+    </View>
+  )
+}
+
+export default EqMapLegend;
+
+const styles = StyleSheet.create({})

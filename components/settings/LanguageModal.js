c5a2d6abc44b42cbac3a864a7bc43fa8fe033968
R-Grigala <r.grigalashvili777@gmail.com>
2023-07-27 18:00:45 +0400

    Coding Continue:// Chnage

--- components/settings/LanguageModal.js
+++ components/settings/LanguageModal.js
@@ -3,11 +3,33 @@ import React, { useState } from 'react';
 
 const { width, height } = Dimensions.get('window');
 
-const LanguageModal = ({langModalVisible, setLangModalVisible}) => {
+const LanguageModal = ({langModalVisible, setLangModalVisible, onSelectLang}) => {
+    const [selectedLang, setSelectedLang] = useState(0);
     const [language, setLanguage] = useState([
         {name: 'English', selected: false},
         {name: 'ქართული', selected: true}
     ]);
+    const onSelect = (index) => {
+        const temp=language;
+        temp.map((item,ind) => {
+            if(index==ind){
+                if(item.selected == true){
+                    item.selected = false
+                }
+                else{
+                    item.selected = true;
+                    setSelectedLang(index);
+                }
+            }else{
+                item.selected = false;
+            }
+        });
+        let temp2 =[]
+        temp.map(item => {
+            temp2.push(item);
+        })
+        setLanguage(temp2);
+    }
   return (
     <Modal
         animationType='slide'
@@ -22,7 +44,11 @@ const LanguageModal = ({langModalVisible, setLangModalVisible}) => {
                 <View style={{width: '100%'}}>
                     <FlatList data={language} renderItem={({item, index}) => {
                         return(
-                            <TouchableOpacity style={styles.languageItem}>
+                            <TouchableOpacity 
+                                style={styles.languageItem}
+                                onPress={() => {
+                                    onSelect(index);
+                                }}>
                                 {item.selected == true?(
                                     <Image 
                                         style={styles.icon}
@@ -40,7 +66,23 @@ const LanguageModal = ({langModalVisible, setLangModalVisible}) => {
                         )
                     }}/>
                 </View>
-
+                <View style={styles.btns}>
+                    <TouchableOpacity 
+                        style={styles.btn1}
+                        onPress={()=>{
+                            setLangModalVisible(false)
+                        }}>
+                        <Text>Calcel</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity 
+                        style={styles.btn2}
+                        onPress={()=>{
+                            setLangModalVisible(true);
+                            onSelectLang(selectedLang);
+                        }}>
+                        <Text style={{color: '#fff'}}>Apply</Text>
+                    </TouchableOpacity>
+                </View>
             </View>
         </View>
     </Modal>
@@ -60,7 +102,7 @@ const styles = StyleSheet.create({
     modalView: {
         margin: 20,
         width: width - 20,
-        height: height / 2,
+        height: height / 3,
         backgroundColor: 'white',
         borderRadius: 20,
         padding: 35,
@@ -91,5 +133,30 @@ const styles = StyleSheet.create({
     icon: {
         width: 24,
         height: 24,
+    },
+    btns: {
+        flexDirection: 'row',
+        width: "100%",
+        justifyContent: 'space-evenly',
+        alignItems: 'center',
+        marginTop: 20,
+        marginBottom: 20,
+    },
+    btn1: {
+        width: '40%',
+        height: 50,
+        borderWidth: 0.5,
+        borderRadius: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    btn2: {
+        width:'40%', 
+        height: 50,
+        borderWidth:0.5,
+        borderRadius:10,
+        backgroundColor: '#4B68E9',
+        justifyContent: 'center',
+        alignItems: 'center',
     }
 })

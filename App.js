3b2ce9af4414ddc558320baec7e5b5d1251cebc9
R-Grigala <r.grigalashvili777@gmail.com>
2023-10-02 16:39:03 +0400

    Coding Grigala:// Change Details

--- App.js
+++ App.js
@@ -5,11 +5,33 @@ import { useEffect, useState } from 'react';
 import { EventRegister } from 'react-native-event-listeners'
 import theme from './theme/theme';
 import themeContext from './theme/themeContext';
+import { Platform } from 'react-native';
+import * as Location from 'expo-location';
 
 export default function App() {
-  const [darkMode, setDarkMode] = useState(false)
+  const [darkMode, setDarkMode] = useState(false);
+  const [locPermission, setLocPermission] = useState('');
+
+  const locationPermission = async () => {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status === "granted") {
+      // Permission to access location was granted
+      alert('Permission to access location was granted');
+    } else {
+      // Permission to access location was denied
+      alert('Permission Not access location was denied');
+    }
+  }
 
   useEffect(() => {
+    const checkPermission = async () => {
+      const status = await locationPermission();
+      setLocPermission(status);
+      console.log(locPermission)
+    };
+
+    checkPermission(); // Initial permission check
+
     const listener = EventRegister.addEventListener('ChangeTheme', (data) =>{
       setDarkMode(data)
       console.log(data)
--- utils/utils.js
+++ utils/utils.js
@@ -36,5 +36,5 @@ export const eqColor = (eqId, origin_time, Id) => {
 
 export const getUrl = () =>{
   // URL of the earthquake image
-  return 'https://887b-212-72-141-34.ngrok-free.app'
+  return 'https://31fd-212-72-141-34.ngrok-free.app'
 }
